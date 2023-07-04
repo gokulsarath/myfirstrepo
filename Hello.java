e4872916cc3960510e4c866f47bca334eb96e217
gokulsarath <35283596+gokulsarath@users.noreply.github.com>
2023-07-04 15:49:45 +0530

    Update Hello.java
    latest commit on remote child branch on Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(" Frustrated in Child"); 
+        System.out.println(" Avatar is coming again"); 
     }
 }
